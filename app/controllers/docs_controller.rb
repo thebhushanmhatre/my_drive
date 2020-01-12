ef7949dbd0fcb394b4c66568c6fe5821adcb99bd
Bhushan C. Mhatre <44663470+thebhushanmhatre@users.noreply.github.com>
2020-01-12 18:41:58 +0530

    Generated Controller Docs and manually added actions and added resourceful routes

---
+++ app/controllers/docs_controller.rb
@@ -0,0 +1,42 @@
+class DocsController < ApplicationController
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+
+  def index
+    
+  end
+
+  def show
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  def destroy
+  end
+
+  private
+
+  def find_doc
+    
+  end
+
+  def docs_params
+    
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  
   root 'welcome#index'
+  
+  get 'welcome/index'
+
+  resources :docs
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
